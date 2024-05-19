6c275de661260e124da3e97653e3b9e60931c469
deonokongo <lilian.deon29@gmail.com>
2024-05-19 11:50:59 -0700

    web_flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,8 +13,9 @@ def index():
 
 
 @app.route('/hbnb', strict_slashes=False)
-    def hbnb():
+def hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
